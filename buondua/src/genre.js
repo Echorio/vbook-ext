ddfdbcea92fcddf130700f06f01d3c3f3de916d6
Echorio <147576557+Echorio@users.noreply.github.com>
2023-10-14 09:40:03 +0700

    Add files via upload

---
+++ buondua/src/genre.js
@@ -0,0 +1,6 @@
+function execute() {
+    return Response.success([
+        {title: "Hot", input: "https://buondua.com/hot/", script: "gen.js"},
+        {title: "Coser", input: "buondua.com/tag/cosplay-10688", script: "gen.js"},
+    ]);
+}
