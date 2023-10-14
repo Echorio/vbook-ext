9f2d8753981bd6cda50e622d05472d40ae76f0a2
Echorio <147576557+Echorio@users.noreply.github.com>
2023-10-14 09:39:25 +0700

    Delete buondua/src/genre.js

--- buondua/src/genre.js
+++
@@ -1,6 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Hot", input: "https://buondua.com/hot/", script: "gen.js"},
-        {title: "Coser", input: "buondua.com/?search=coser", script: "gen.js"},
-    ]);
-}
